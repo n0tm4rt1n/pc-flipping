0b45830b10157f42bab0823dc9ea218316de5e1f
Martin Ananiev <martin.ananiev04@hotmail.com>
2021-09-30 17:17:36 +0300

    commit with frontend

---
+++ frontend/makereq.js
@@ -0,0 +1,8 @@
+function searchElement () {
+    var card = document.getElementById('cardtype').value
+    var vrm = document.getElementById('vram').value
+    var manif = document.getElementById('manifacturer').value
+
+    console.log(card, vrm, manif);
+}
+
