dd7455cd3aa2cd1a4e10774ba9b4617a57f7f142
Martin Ananiev <martin.ananiev04@hotmail.com>
2021-09-30 20:21:29 +0300

    spelling error :)

--- frontend/makereq.js
+++ frontend/makereq.js
@@ -1,7 +1,7 @@
 function searchElement () {
     var card = document.getElementById('cardtype').value
     var vrm = document.getElementById('vram').value
-    var manif = document.getElementById('manifacturer').value
+    var manif = document.getElementById('manufacturer').value
 
     console.log(card, vrm, manif);
 }
